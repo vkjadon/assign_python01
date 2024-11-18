3ec433fd983c5d684205bafedc8442494a4b2a49
Vijay K Jadon <37266244+vkjadon@users.noreply.github.com>
2024-11-18 14:37:44 +0530

    Create student_code.py

---
+++ student_code.py
@@ -0,0 +1,9 @@
+# Implement the required functions below.
+
+def add_numbers(a, b):
+    # TODO: Implement this function
+    pass
+
+def multiply_numbers(a, b):
+    # TODO: Implement this function
+    pass
