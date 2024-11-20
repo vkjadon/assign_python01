a4b117d57bbbbb4579a7d053e8b117c7ad2f915d
Vijay K Jadon <37266244+vkjadon@users.noreply.github.com>
2024-11-20 12:02:39 +0530

    Update test_cases.py

--- test_cases.py
+++ test_cases.py
@@ -8,13 +8,6 @@ def test_add_numbers():
     assert add_numbers(-1, 1) == 0
     assert add_numbers(0, 0) == 0
 
-def test_subtract_numbers():
-    assert subtract_numbers(2, 3) == -1
-    assert subtract_numbers(-1, 3) == -4
-    assert subtract_numbers(0, 10) == -10
-    assert subtract_numbers(5, 1) == 4
-    assert subtract_numbers(5, 5) == 0
-
 def test_multiply_numbers():
     assert multiply_numbers(2, 3) == 6
     assert multiply_numbers(-1, 3) == -3
